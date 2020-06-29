86f752366cd6f31bdc83419a518e58e60fcca0c2
Dan Steren <daniel.d.steren@gmail.com>
2020-06-28 20:35:04 -0600

    Post response as non-ephemeral message

--- functions/interactions/interactions.js
+++ functions/interactions/interactions.js
@@ -5,20 +5,30 @@ const slackTextParser = require('./middleware/slackTextParser');
 function interactions(event, context, callback) {
   if (event.body.payload.actions[0].action_id === 'slash_init_users_selected') {
     const selectedUsers = event.body.payload.actions[0].selected_users;
-    const rotation = selectedUsers.map((userId) => `<@${userId}>`).join(', ');
-
-    axios
-      .post(
-        event.body.payload.response_url,
-        {
-          replace_original: 'true',
-          text: `Success! This channel has been configured with the following rotation: ${rotation}`,
-        },
-        {
-          headers: { 'Content-type': 'application/json' },
-        }
-      )
-      .catch(() => {});
+    if (selectedUsers.length === 0) {
+      axios
+        .post(
+          event.body.payload.response_url,
+          { delete_original: 'true' },
+          { headers: { 'Content-type': 'application/json' } }
+        )
+        .catch(() => {});
+    } else {
+      const rotation = selectedUsers.map((userId) => `<@${userId}>`).join(', ');
+      axios
+        .post(
+          event.body.payload.response_url,
+          {
+            delete_original: 'true',
+            response_type: 'in_channel',
+            text: `This channel has been configured with the following on-call rotation: ${rotation}\nTo manage this schedule use the \`/tribute\` command.`,
+          },
+          {
+            headers: { 'Content-type': 'application/json' },
+          }
+        )
+        .catch(() => {});
+    }
     callback(null, {
       statusCode: 200,
       body: 'OK',
